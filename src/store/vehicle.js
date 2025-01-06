3b6dc657a95b9c62183a2d10f81e7b6ae4b8ac94
Eduarduar <eduardoarcega1997@gmail.com>
2025-01-06 10:32:36 -0600

    add: Crear archivo vehicle.js con tipos y datos de vehículos

---
+++ src/store/vehicle.js
@@ -0,0 +1,40 @@
+export const types = ["Auto", "Truck", "Bus", "Rv", "Motorcycle"];
+
+export const vehicle = [
+  {
+    image: "assets/img/2AxlesTruck.png",
+    type: "Truck",
+    axles: 2,
+    description: "Camión de 2 ejes",
+  },
+  {
+    image: "assets/img/3AxlesTruck.png",
+    type: "Truck",
+    axles: 3,
+    description: "Camión de 3 ejes",
+  },
+  {
+    image: "assets/img/4AxlesTruck.png",
+    type: "Truck",
+    axles: 4,
+    description: "Camión de 4 ejes",
+  },
+  {
+    image: "assets/img/5AxlesTruck.png",
+    type: "Truck",
+    axles: 5,
+    description: "Camión de 5 ejes",
+  },
+  {
+    image: "assets/img/6AxlesTruck.png",
+    type: "Truck",
+    axles: 6,
+    description: "Camión de 6 ejes",
+  },
+  {
+    image: "assets/img/7AxlesTruck.png",
+    type: "Truck",
+    axles: 7,
+    description: "Camión de 7 ejes",
+  },
+];
